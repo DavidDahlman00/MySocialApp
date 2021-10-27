0de15e82c0b7d1ba02e03c8684017b852bccd456
David Dahlman <daviddahlman82@gmail.com>
2021-10-27 13:56:48 +0200

    fix bug

--- app/src/main/java/com/example/myapplicati/StartActivity.kt
+++ app/src/main/java/com/example/myapplicati/StartActivity.kt
@@ -2,10 +2,30 @@ package com.example.myapplicati
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
+    private val contactFragment = ContactList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        val mapFragment = supportFragmentManager
+        switchFragment(contactFragment)
+
+        start_navigation_bar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.maps -> switchFragment(contactFragment)
+
+            }
+            true
+        }
+    }
+    private fun switchFragment(fragment: Fragment){
+        if (fragment != null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.start_fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
